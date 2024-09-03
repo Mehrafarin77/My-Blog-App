da3e782aaf5f3bdb51a57d4fe382da90bf129809
Mehrafarin <fatemehehooshangi79@gmail.com>
2024-09-03 11:34:35 +0330

    changes commited

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
+  # + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
